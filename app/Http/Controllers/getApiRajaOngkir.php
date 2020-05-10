d4dc0979d49a11ba9fc7c34dca68bd655bdc0e5f
hilmibukhori <hilmibukhori5@gmail.com>
2020-05-10 08:18:44 +0700

    get province

---
+++ app/Http/Controllers/getApiRajaOngkir.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class getApiRajaOngkir extends Controller
+{
+    public function checkout(){
+        //mengarah kepada file checkout.blade.php yang ada di view
+        return view('checkout');
+    }
+
+    public function get_province(){
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+        CURLOPT_URL => "https://api.rajaongkir.com/starter/province",
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 30,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "GET",
+        CURLOPT_HTTPHEADER => array(
+            "key: 0df6d5bf733214af6c6644eb8717c92c"
+        ),
+        ));
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+
+        curl_close($curl);
+
+        if ($err) {
+            echo "cURL Error #:" . $err;
+        } else {
+            //ini kita decode data nya terlebih dahulu
+            $response=json_decode($response,true);
+            //ini untuk mengambil data provinsi yang ada di dalam rajaongkir resul
+            $data_pengirim = $response['rajaongkir']['results'];
+            return $data_pengirim;
+        }
+    }
+    public function get_city(){
+    }
+}
--- config/app.php
+++ config/app.php
@@ -207,6 +207,7 @@ return [
         'File' => Illuminate\Support\Facades\File::class,
         'Gate' => Illuminate\Support\Facades\Gate::class,
         'Hash' => Illuminate\Support\Facades\Hash::class,
+        'Http' => Illuminate\Support\Facades\Http::class,
         'Lang' => Illuminate\Support\Facades\Lang::class,
         'Log' => Illuminate\Support\Facades\Log::class,
         'Mail' => Illuminate\Support\Facades\Mail::class,
@@ -225,6 +226,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        
 
     ],
 
